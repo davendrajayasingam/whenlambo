c47b9e5f45aa4bd4c62a569fbb62c76427047770
Davendra Jayasingam <hello@davendra.me>
2023-09-06 03:40:03 +0800

    Trying with Binance API Key

--- app/api/[exchangeId]/[currencyId]/route.ts
+++ app/api/[exchangeId]/[currencyId]/route.ts
@@ -25,7 +25,10 @@ export async function GET(req: NextRequest, { params }: { params: { exchangeId: 
     if (exchange === 'BINANCE')
     {
         const price = await fetch(`https://api.binance.com/api/v3/ticker/bookTicker?symbol=${currency}USDT`, {
-            next: { revalidate: 5 }
+            next: { revalidate: 5 },
+            headers: {
+                'X-MBX-APIKEY': process.env.BINANCE_API_KEY!
+            }
         })
             .then(res => res.json())
             .then(data =>
